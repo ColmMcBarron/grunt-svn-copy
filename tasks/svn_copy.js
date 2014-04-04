05ee74ffbad793095e072d59939b7ab2c11c294b
Colm McBarron <colm.mcbarron@iqcontent.com>
2014-04-04 15:03:11 +0100

    0.1.1

--- tasks/svn_copy.js
+++ tasks/svn_copy.js
@@ -38,49 +38,6 @@ module.exports = function(grunt) {
       });
     });
 
-
-      
-
-//    var done = this.async();
-//    var command = [ options.bin, 'add', options.files].join(' ');
-/*
-  exec(command, options.execOpts, function (error, stdout) {
-      grunt.log.write(stdout);
-      if (error !== null) {
-        grunt.log.error('\n#' + command + "\n" + error);
-      }
-      done(true);
-    });
-*/
-/*
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        //if (!grunt.file.exists(filepath)) {
-        //  grunt.log.warn('Source file "' + filepath + '" not found.');
-        // / return false;
-       // } else {
-       //   return true;
-       // }
-      }).map(function(filepath) {
-        // Read file source.
-     //   var command = [ options.bin, 'add', options.files].join(' ');
-     //   return grunt.file.read(filepath);
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-//      src += options.punctuation;
-
-      // Write the destination file.
-//      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-*/
   });
 
 };
